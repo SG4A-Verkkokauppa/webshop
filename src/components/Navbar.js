4112cf05af0a97d4ece5c88357105f945b25effa
Taskulaatikko <n2hasa@students.oamk.fi>
2023-03-15 14:24:26 +0200

    Valikon nimien muuttaminen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,28 +17,29 @@ export default function Navbar() {
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link active" aria-current="page" href="#">Etusivu</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+              Tuotteet
             </a>
             <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Paidat</a></li>
+              <li><a class="dropdown-item" href="#">Kynät</a></li>
+              <li><a class="dropdown-item" href="#">Juliste</a></li>
               <li>
                 <hr class="dropdown-divider"/>
               </li>
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Yhteystiedot</a>
+          </li>
         </ul>
         <form class="d-flex mt-3" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-success" type="submit">Search</button>
+          <button class="btn btn-success" type="submit">Etsi</button>
         </form>
       </div>
     </div>
