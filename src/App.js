165aff436cbbf733fc4752a5f7b143974ff8f915
Anne Pyykkonen <n0pyan00@students.oamk.fi>
2023-03-15 16:56:11 +0200

    Karuselli toimii, mutta on iso

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import HomeCarousel from "./HomeCarousel/HomeCarousel";
 
 function App() {
   return (
-  <>
+ 
+ <>  <div className='navbar'>
   <Navbar/>
-  </>
+  </div>
+  
+
+<div className="app">
+<HomeCarousel/>
+</div>
+
+</>
+
   );
 }
 
---
+++ src/HomeCarousel/HomeCarousel.js
@@ -0,0 +1,37 @@
+
+import React, {useState} from "react"; 
+import Carousel from 'react-bootstrap/Carousel';
+
+const data = [
+  {
+   image: require('./images/lehti.jpg'), 
+  },
+  {
+    image:require('./images/silta.jpg'), 
+   }
+]
+
+function HomeCarousel() {
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+       {data.map((slide, i) => {
+        return (
+          <Carousel.Item>        
+        <img
+          className="d-block w-100"
+          src={slide.image}
+          alt="slider image"
+        />
+      </Carousel.Item>
+        )
+      })}
+      
+    </Carousel>
+  );
+}
+export default HomeCarousel;
