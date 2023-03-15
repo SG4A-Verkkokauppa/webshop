ac9e57d00f2e23f809f3e7147be5d8a6244aa5c2
Taskulaatikko <n2hasa@students.oamk.fi>
2023-03-15 14:41:54 +0200

    karuselli filejä poistettu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Carousel from './components/Carousel';
 
 function App() {
   return (
   <>
   <Navbar/>
-  <Carousel/>
   </>
   );
 }
