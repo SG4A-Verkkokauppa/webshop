808e2efe8cce76cd3bc9b63e41b9d2d51b0a031d
Taskulaatikko <n2hasa@students.oamk.fi>
2023-03-17 12:33:11 +0200

    karusellin korkeus tottelee

--- src/HomeCarousel/HomeCarousel.js
+++ src/HomeCarousel/HomeCarousel.js
@@ -18,6 +18,7 @@ function HomeCarousel() {
   };
 
   return (
+    
     <Carousel activeIndex={index} onSelect={handleSelect}>
        {data.map((slide, i) => {
         return (
@@ -32,6 +33,8 @@ function HomeCarousel() {
       })}
       
     </Carousel>
+    
   );
 }
+
 export default HomeCarousel;
