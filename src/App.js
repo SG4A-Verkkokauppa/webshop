8b44516e851cccb3c457e7489e2098950229498b
Taskulaatikko <n2hasa@students.oamk.fi>
2023-03-22 09:33:40 +0200

    Uusia filejä luotu ja carttia koitettu linkitellä

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home"
 
-
+//const URL ='http://localhost:8888/maagista/';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    if ('cart' in localStorage) {
+      setCart(JSON.parse(localStorage.getItem('cart')));
+    }
+   }, [])
+
+
+
+
   return (
     <>
 
