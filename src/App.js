245bd355401c3b59cae05f1db85e69466d46a71f
Taskulaatikko <n2hasa@students.oamk.fi>
2023-03-22 09:40:03 +0200

    säätöä

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home"
-import { Row, Col, Container } from "react-bootstrap";
-import GridCard from "./components/GridCard";
 import Footer from "./components/Footer";
 import React from "react";
 
