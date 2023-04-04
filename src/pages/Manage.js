649bdf8cd1557ed395f4e6a9c0c1632191c2429e
MilaKim02 <n2kimi01@students.oamk.fi>
2023-04-04 15:12:57 +0300

    ylläpitosivu frontend koodi

---
+++ src/pages/Manage.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+export default function Manage({url}) {
+    const [newCategory, setNewCategory] = useState("")
+    const [selectedCategory,setSelectedCategory ] = useState(null)
+    const [addingCategory, setAddingCategory] = useState(false)
+
+    function saveCategory(e) {
+        e.preventDefault();
+        const json = JSON.stringify({name: newCategory});
+        axios.post(url + 'products/addcategory.php',json,{
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+        .then((response) => {
+            setNewCategory('');
+            setAddingCategory(false);
+            setSelectedCategory(response.data);
+        }).catch(error => {
+            alert(error.response === undefined ? error : error.response.data.error);
+        });
+    }
+
+    if (!addingCategory) {
+        return (
+            <>
+            <h3>Tuoteryhmien ylläpito</h3>
+            <div>
+                <label>ylläpito</label>
+                <CategoryList
+                url={url}
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+                />
+                <button className="btn btn-dark" type="button" onClick={()=> setAddingCategory(true)}>Add</button>
+            </div>
+        </>
+        )
+    } else {
+        return (
+            <>
+            <h3>Lisää uusi tuoteryhmä</h3>
+            <form onSubmit={saveCategory}>
+                <div>
+                    <label>Tuoteryhmän nimi</label>
+                    <input type="text" value={newCategory} onChange={(e) => setNewCategory(e.target.value)}></input>
+                </div>
+                <button type="button" onClick={() => setAddingCategory(false)}>Peruuta</button>
+                <button type="submit">Tallenna</button>
+            </form>
+            </>
+        )
+    }
+  
+}
