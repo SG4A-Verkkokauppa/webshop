6533988dde8f04624dce6d6454a4426a6d27a5a1
MilaKim02 <n2kimi01@students.oamk.fi>
2023-04-19 17:41:29 +0300

    säpö osoite päivitetty footeriin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,12 +13,12 @@ export default function Footer() {
             <tr>
                 <th>MAAGISTA.FI OY</th>
                 <th>OTA YHTEYTTÄ</th>
-                <th><a href=''> Toimitus</a></th>
+                <th> Toimitus</th>
             </tr>
             <tr>
                 <td>Meemimäki 7</td>
-                <td>meemila@meemi.com</td>
-                <td><a href=''> Maksutavat</a></td>
+                <td>asiakaspalvelu@maagista.fi</td>
+                <td> Maksutavat</td>
             </tr>
             <tr>
                 <td>123456 Meemilä</td>
@@ -27,15 +27,7 @@ export default function Footer() {
                       to={'/Manage'}>Ylläpito
                     </Link>}</td>
             </tr>
-            <tr>
-               <td></td>
-               <td></td>
-               </tr>
-
-            <tr>
-               <td></td>
-               <td></td>
-            </tr>
+          
 
     </table>
     </div>
