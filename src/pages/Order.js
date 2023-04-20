5c86d1608c57193249d16c1bb7bccc127c731c8d
Taskulaatikko <n2hasa@students.oamk.fi>
2023-04-20 10:01:25 +0300

    parempi

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -135,7 +135,7 @@ export default function Order({cart, removeFromCart, updateAmount, changeAmount}
     </div>
   
     <div>
-    <button onClick={(e) => this.deleteRow(id, e)}>Tilaa</button>
+
     </div>
     </form>
     </>
