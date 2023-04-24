2beea32eb1d8f5123581600d1f7295d160a92f0d
Anne Pyykkonen <n0pyan00@students.oamk.fi>
2023-04-24 16:29:38 +0300

    Etusivun korttien linkit toimii

--- src/components/GridCard.js
+++ src/components/GridCard.js
@@ -4,12 +4,17 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import "../App.css";
 
 function GroupExample() {
+  
   return (
     <CardGroup>
       <Card>
-        <Card.Img variant="top" src={require("./images/muki-pinkki-maaginen.png")} />
+      <Card.Link href="/product/5">
+        <Card.Img  variant="top" src={require("./images/muki-pinkki-maaginen.png")} />
+        </Card.Link>
         <Card.Body>
+        <Card.Link href="/product/5">
           <Card.Title>Maaginen muki</Card.Title>
+          </Card.Link>
           <Card.Text>
           Maistele maagisen hyvät juomat tästä mukista.
           </Card.Text>
@@ -17,9 +22,11 @@ function GroupExample() {
 
       </Card>
       <Card>
+      <Card.Link href="/product/1">
         <Card.Img variant="top" src={require("./images/paita-pinkki-maaginen.png")} />
+        </Card.Link>
         <Card.Body>
-          <Card.Title>Maaginen paita</Card.Title>
+        <Card.Link href="/product/1"><Card.Title>Maaginen paita</Card.Title></Card.Link>
           <Card.Text>
           Tämä paita hivelee maagisesti muotojasi.
           </Card.Text>
@@ -27,9 +34,11 @@ function GroupExample() {
 
       </Card>
       <Card>
+      <Card.Link href="/product/10">
         <Card.Img variant="top" src={require("./images/parkkikiekko-pinkki-muumilimu.png")} />
+        </Card.Link>
         <Card.Body>
-          <Card.Title>Maaginen parkkikiekko</Card.Title>
+        <Card.Link href="/product/10"><Card.Title>Maaginen parkkikiekko</Card.Title></Card.Link>
           <Card.Text>
           Välty maagisen suurilta parkkisakoilta.
           </Card.Text>
